e10fc7371db035a564dcb3c8a9dbd8a06700ad40
mrfhmd7 <mrf.hmd7@gmail.com>
2025-06-04 21:11:09 +0600

    add and update component added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './components/addCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "add-coffee",
+        element: <AddCoffee />
+    },
+    {
+        path: "update-coffee",
+        element: <UpdateCoffee />,
+    },
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
