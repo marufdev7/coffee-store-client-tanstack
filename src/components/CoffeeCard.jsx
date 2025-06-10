a99142979dab0ee0108ecff374d4baa72154b59e
mrfhmd7 <mrf.hmd7@gmail.com>
2025-06-10 10:53:49 +0600

    get alert for delete

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, chef, supplier, test, category, photo, details } = coffee;
+    const { _id, name, chef, supplier, test, category, photo, details } = coffee;
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Swal.fire({
+                //     title: "Deleted!",
+                //     text: "Your file has been deleted.",
+                //     icon: "success"
+                // });
+                console.log('delete confirmed');
+            }
+        });
+    }
+
     return (
         <div className="card card-side bg-base-100 shadow-xl">
             <figure>
@@ -11,8 +35,8 @@ const CoffeeCard = ({ coffee }) => {
             <div className="flex justify-between items-center w-full">
                 <div>
                     <h2 className="card-title">{name}</h2>
-                    <p>{chef}</p>
-                    <p>{supplier}</p>
+                    {/* <p>{chef}</p> */}
+                    {/* <p>{supplier}</p> */}
                     <p>{test}</p>
                     <p>{category}</p>
                     <p>{details}</p>
@@ -21,7 +45,10 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="join join-vertical space-y-3">
                         <button className="btn  rounded-md">View</button>
                         <button className="btn rounded-md">Edit</button>
-                        <button className="btn rounded-md">X</button>
+                        <button
+                            onClick={() => handleDelete(_id)}
+                            className="btn rounded-md bg-red-400">
+                            X</button>
                     </div>
                 </div>
             </div>
